d286db811c47c026c257f31d2669704bb39d7717
ianbautista <ianfbautista@gmail.com>
2020-09-03 22:37:54 -0700

    TASK http://localhost:2020/names/start/u

--- src/main/java/com/lambdaschool/javacountries/controllers/CountryController.java
+++ src/main/java/com/lambdaschool/javacountries/controllers/CountryController.java
@@ -18,12 +18,24 @@ public class CountryController
     @Autowired
     private CountryRepository countryrepos;
 
+    private List<Country> findCountries(List<Country> list, CheckCountry tester)
+    {
+        List<Country> tempList = new ArrayList<>();
+        for (Country c : list)
+        {
+            if (tester.test(c))
+            {
+                tempList.add(c);
+            }
+        }
+        return tempList;
+    }
+
     //http://localhost:2020/names/all
     @GetMapping(value = "/names/all", produces = {"application/json"})
     public ResponseEntity<?> listAllCountries()
     {
         List<Country> allCountryList = new ArrayList<>();
-
         countryrepos.findAll().iterator().forEachRemaining(allCountryList::add);
 
         allCountryList.sort((c1, c2) -> c1.getName().compareToIgnoreCase(c2.getName()));
@@ -36,5 +48,15 @@ public class CountryController
         return new ResponseEntity<>(allCountryList, HttpStatus.OK);
     }
 
+    //http://localhost:2020/names/start/u
+    @GetMapping(value = "/names/start/{letter}", produces = {"application/json"})
+    public ResponseEntity<?> listAllByFirstLetter(@PathVariable char letter)
+    {
+        List<Country> allCountryList = new ArrayList<>();
+        countryrepos.findAll().iterator().forEachRemaining(allCountryList::add);
+
+        List<Country> returnList = findCountries(allCountryList, c -> c.getName().toLowerCase().charAt(0) == letter);
+        return new ResponseEntity<>( returnList, HttpStatus.OK);
+    }
 
 }
